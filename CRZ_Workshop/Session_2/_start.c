f32f4d516f524fbde0d4782891333baf2c5103a5
Rohan S Khadd <rohanskhadd@gmail.com>
2025-11-21 00:29:56 +0530

    WorkSHop_7

--- CRZ_Workshop/Session_2/_start.c
+++ CRZ_Workshop/Session_2/_start.c
@@ -8,14 +8,5 @@ void _start (void);
 //body
 void __attribute__ ((section(".after_vectors"),noreturn,weak))
 _start (void){
-
-    asm volatile
-    (
-        " ldr     r0,=_start \n"
-        " bx      r0"
-        :
-        :
-        :
-    );
-
+    main();
 }
